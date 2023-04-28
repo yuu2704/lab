638f98d83449587d53109f8c80c50e8af46438f6
yuu2704 <69102974+yuu2704@users.noreply.github.com>
2023-04-28 16:02:11 +0900

    Delete kadai3b/fast-style-transfer directory

--- kadai3b/fast-style-transfer/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from models.autoencoder import Autoencoder
-from models.autoencoder_dispatched import AutoencoderOld
-from models.autoencoder_attention import AutoencoderAttention
-from models.bottleNet import BottleNetwork
-from models.denseNet import DenseNet
-from models.resNext import ResNext
